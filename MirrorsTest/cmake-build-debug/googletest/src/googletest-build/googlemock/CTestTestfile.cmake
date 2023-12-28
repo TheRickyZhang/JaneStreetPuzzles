c16b4ea33f0a20a82dbbb7689ffa60dc411e835a
Ricky Zhang <rickyzhang196@outlook.com>
2023-12-28 15:27:37 -0500

    Move MirrorsTest to Archive

--- MirrorsTest/cmake-build-debug/googletest/src/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ricky/Projects/JaneStreet/MirrorsTest/cmake-build-debug/googletest/src/googletest/googlemock
-# Build directory: C:/Users/ricky/Projects/JaneStreet/MirrorsTest/cmake-build-debug/googletest/src/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- MirrorsTest/cmake-build-debug/googletest/src/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ricky/Projects/JaneStreet/MirrorsTest/cmake-build-debug/googletest/src/googletest/googletest
-# Build directory: C:/Users/ricky/Projects/JaneStreet/MirrorsTest/cmake-build-debug/googletest/src/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
